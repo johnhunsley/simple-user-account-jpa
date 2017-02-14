750674cc3c3f1a72f17b5ca3afd2a1b6d3d8e6b3
johnhunsley <snowball33>
2017-02-14 13:54:25 +0000

    pageAll test

--- src/test/java/com/johnhunsley/user/jpa/UserDetailsServiceJpaImplTest.java
+++ src/test/java/com/johnhunsley/user/jpa/UserDetailsServiceJpaImplTest.java
@@ -1,5 +1,6 @@
 package com.johnhunsley.user.jpa;
 
+import com.johnhunsley.user.jpa.domain.UserJpaImpl;
 import com.johnhunsley.user.jpa.repository.UserRepositoryJpaImpl;
 import com.johnhunsley.user.jpa.service.UserDetailsServiceJpaImpl;
 import com.johnhunsley.user.service.UserDetailsServiceImpl;
@@ -8,10 +9,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * @author John Hunsley
@@ -36,7 +46,21 @@ public class UserDetailsServiceJpaImplTest {
 
     @Test
     public void testPageAllUser() {
-        assertTrue(true); //todo write this
+        final long totalItems = 10L;
+        final int totalPages = 2;
+        UserJpaImpl user = new UserJpaImpl();
+        List<UserJpaImpl> users = new ArrayList<>();
+        users.add(user);
+        org.springframework.data.domain.Page<UserJpaImpl> mockPage = Mockito.mock(Page.class);
+        when(mockPage.getTotalElements()).thenReturn(totalItems);
+        when(mockPage.getTotalPages()).thenReturn(totalPages);
+        when(mockPage.getContent()).thenReturn(users);
+        when(userRepository.findAll(any(Pageable.class))).thenReturn(mockPage);
+
+        com.johnhunsley.user.domain.Page page = userDetailsService.pageAllUser(5,1);
+        assertTrue(page.getTotalItems() == totalItems);
+        assertTrue(page.getTotalPages() == totalPages);
+        assertFalse(page.getPagedItems().isEmpty());
     }
 
 }
